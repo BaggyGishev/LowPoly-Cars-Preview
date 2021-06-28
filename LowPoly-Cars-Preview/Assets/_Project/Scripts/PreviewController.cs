49708c3e49609a2922be878170a6107ae73935cf
Gisha <gishathedev@gmail.com>
2021-06-28 22:10:24 +0300

    add passive camera rotation

--- LowPoly-Cars-Preview/Assets/_Project/Scripts/PreviewController.cs
+++ LowPoly-Cars-Preview/Assets/_Project/Scripts/PreviewController.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class PreviewController : MonoBehaviour
 {
+    [SerializeField] private float rotateSpeed;
 
+
+    private void Update()
+    {
+        transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
+    }
 }
